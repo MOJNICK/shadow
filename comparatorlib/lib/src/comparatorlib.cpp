d8f7064fe229621ea50a7485042f93620dff9647
MOJNICK <szymonszozda@gmail.com>
2017-10-27 00:12:48 +0200

    modified:   comparatorlib/lib/src/comparatorlib.cpp fun implemented

--- comparatorlib/lib/src/comparatorlib.cpp
+++ comparatorlib/lib/src/comparatorlib.cpp
@@ -1,76 +1,72 @@
 #include "comparatorlib.hpp"
-const int channels = 3;//only for convinience
-double prealocate = 0.01;
+int const channels = 3;//only for convinience
+double const prealocate = 0.01;
+
 
 	template <class pTYPE>
-	std::vector<cv::Point_<int>> iterate_H(cv::Mat img, void (*classifier)(pTYPE, pTYPE, double, double, cv::Point3_), double lightThreshold, double colorThreshold, cv::Point3_ colorBalance = cv::Point3_(1, 1, 1))
+	std::vector<int> iterate_H(cv::Mat img, void (*classifier)(pTYPE, pTYPE, double, double, cv::Point3_), double lightThreshold, double colorThreshold, cv::Point3_ colorBalance = cv::Point3_(1, 1, 1))
 	{
-		std::vector<cv::Point_<int>> result;
-		result.reserve(sizeof(cv::Point_<int>) * img.total() * prealocate)
-		if(img.isContinuous())
+		std::vector<int> result;
+		result.reserve(sizeof(int) * img.total() * prealocate);
+
+		for(int i = 0; i < img.rows; i++)		
 		{
-			for(int i = 0; i < img.rows; i++)		
-				for(int j = i * img.cols; j < i * img.cols - channels; j += channels)
+			int rowIndex = i * img.step;
+			for(int j = 0; j < img.cols - channels; j += channels)
+			{
+				switch (classifier(img[rowIndex + j], img[rowIndex + j + channels], lightThreshold, colorThreshold, colorBalance))
 				{
-					switch (classifier(img[j], img[j + channels], lightThreshold, colorThreshold, colorBalance))
-					{
-						// case no: continue;
-						// case forward: result.push_back(cv::Point_<int>(i, j + channels));
-						// case backward: result.push_back(cv::Point_<int>(i, j));
-					}
+					case no: continue;
+					case backward: result.push_back(rowIndex + j);
+					case forward: result.push_back(rowIndex + j + channels);
 				}
+			}
 		}
-		else
-		{
-			//,at
-		}
+
 		return result;
 	}
 	
 	template <class pTYPE>
-	std::vector<cv::Point_<int>> iterate_V(cv::Mat img, void (*classifier)(pTYPE, pTYPE, double, double, cv::Point3_), double lightThreshold, double colorThreshold, cv::Point3_ colorBalance = cv::Point3_(1, 1, 1))
+	std::vector<int> iterate_V(cv::Mat img, void (*classifier)(pTYPE, pTYPE, double, double, cv::Point3_), double lightThreshold, double colorThreshold, cv::Point3_ colorBalance = cv::Point3_(1, 1, 1))
 	{
-		std::vector<cv::Point_<int>> result;
-		result.reserve(sizeof(cv::Point_<int>) * img.total() * prealocate)
-		if(img.isContinuous())
+		std::vector<int> result;
+		result.reserve(sizeof(int) * img.total() * prealocate)
+
+		for(int col = 0; col < img.cols - channels; col += channels)		
 		{
-			for(int j = 0; j < img.cols - channels; j += channels)		
-				for(int i = j; i < j * img.rows - img.cols; i+=img.cols)
+			for(int row = 0; row < img.rows - 1; row++)
+			{
+				switch (classifier(img[row * img.step + col], img[((row + 1) * img.step) + col], lightThreshold, colorThreshold, colorBalance))
 				{
-					switch (classifier(img[i], img[i + img.cols], lightThreshold, colorThreshold, colorBalance))
-					{
-						// case no: continue;
-						// case forward: result.push_back(cv::Point_<int>(i, j + channels));
-						// case backward: result.push_back(cv::Point_<int>(i, j));
-					}
+					case no: continue;
+					case forward: result.push_back(((row + 1) * img.step) + col);
+					case backward: result.push_back(row * img.step + col);
 				}
-		}
-		else
-		{
-			//,at
+			}
 		}
 		return result;
 	}
 
 	template <class pTYPE>
-	void iterate_HV(cv::Mat img, void (*)(pTYPE, pTYPE, double, double, cv::Point3_))
+	std::vector<int> iterate_HV(cv::Mat img, void (*classifier)(pTYPE, pTYPE, double, double, cv::Point3_), double lightThreshold, double colorThreshold, cv::Point3_ colorBalance = cv::Point3_(1, 1, 1))
 	{
-
+		auto shadowH = iterate_H(img, classifier, lightThreshold, colorThreshold, colorBalance);
+		auto shadowV = iterate_V(img, classifier, lightThreshold, colorThreshold, colorBalance);
+		shadowH.insert(shadowH.end(), shadowV.begin(), shadowV.end());
+		auto shadowHV = shadowH;
+		return shadowHV;
 	}
-
-	template <class pTYPE>
-	vectorlPoint_<int> save_result()
 
 	template <class pTYPE>
 	Transition color_light_classifier(pTYPE pix0in, pTYPE pix1in, double lightThreshold, double colorThreshold, cv::Point3_ colorBalance = cv::Point3_(1, 1, 1))
 	{
-		pTYPE pix0 = new (pTYPE*)[3]; std::memcpy(pix0, pix0in, 3 * sizeof(pTYPE*));//no impact at source image
-		pTYPE pix1 = new (pTYPE*)[3]; std::memcpy(pix1, pix1in, 3 * sizeof(pTYPE*));//no impact at source image
+		pTYPE pix0 = new (pTYPE*)[channels]; std::memcpy(pix0, pix0in, channels * sizeof(pTYPE*));//no impact at source image
+		pTYPE pix1 = new (pTYPE*)[channels]; std::memcpy(pix1, pix1in, channels * sizeof(pTYPE*));//no impact at source image
 
 		Transition result = forward;
 		if(brighter(pix0, pix1))
 		{
-			swap_pointer(&pix0, &pix1);//pix0 is now the dimm
+			std::swap(pix0, pix1);//pix0 is now the dimmer
 			result = backward;
 		}
 
@@ -92,14 +88,6 @@ double prealocate = 0.01;
 	int color_distance(cv::Point3_ colorBalance, pTYPE pix0, pTYPE pix1)
 	{
 		return pow(pix0[0] - pix1[0], 2) + pow(pix0[1] - pix1[1], 2) + pow(pix0[2] - pix1[2], 2);
-	}
-
-	template <class pTYPE>
-	void swap_pointer(pTYPE* p0, pTYPE* p1)
-	{
-		pTYPE* tmp = p0;
-		*p0 = *p1;
-		*p1 = *tmp;
 	}
 
 	template<class pTYPE>
