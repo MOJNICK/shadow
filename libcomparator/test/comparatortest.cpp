b9c087e00415778128e80569560db0e17e0417a3
MOJNICK <szymonszozda@gmail.com>
2017-10-28 23:43:36 +0200

    ASSERT_EQ

--- libcomparator/test/comparatortest.cpp
+++ libcomparator/test/comparatortest.cpp
@@ -18,14 +18,12 @@ protected:
       int pix1[] = {3, 3, 3};
       
       classifier.copy_pix(pix0, pix1);
-      if (Transition::no != classifier.f_classifier())
-          ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_OUTPUT);
+      ASSERT_EQ(Transition::no, classifier.f_classifier());
 
       std::fill( pix0, pix0 + channels, 3);
       std::fill( pix1, pix1 + channels, 10);
       classifier.copy_pix(pix0, pix1);
-      if (Transition::forward != classifier.f_classifier())
-          ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_OUTPUT);
+      ASSERT_EQ(Transition::forward, classifier.f_classifier());
 
       std::fill( pix0, pix0 + channels, 10);
       std::fill( pix1, pix1 + channels, 3);
@@ -42,6 +40,55 @@ protected:
 TEST(ComparatorLibSuite, f_classifier) {
   Fclassifier fclassifeir;
   fclassifeir.safe_run();
+}
+
+
+class FclassifierBT : public cvtest::BaseTest
+{
+public:
+    FclassifierBT(){}
+protected:
+    
+    void run(int) {
+      ts->set_failed_test_info(cvtest::TS::OK);
+
+      double balance[] = {2.0, 1.0, 1.0};
+      Classifier<int> classifier(1.0, 4.0, balance);
+
+      int pix0[] = {3, 3, 3};
+      int pix1[] = {3, 3, 3};
+      
+      classifier.copy_pix(pix0, pix1);
+      if (Transition::no != classifier.f_classifier())
+          ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_OUTPUT);
+
+      std::fill( pix0, pix0 + channels, 6);
+      std::fill( pix1, pix1 + channels, 10);
+      pix0[0] = 4;      
+      classifier.copy_pix(pix0, pix1);
+      ASSERT_EQ(Transition::forward, classifier.f_classifier());
+
+      std::fill( pix0, pix0 + channels, 10);
+      std::fill( pix1, pix1 + channels, 3);
+      classifier.copy_pix(pix0, pix1);
+      ASSERT_EQ(Transition::no, classifier.f_classifier());
+
+      // std::fill( pix0, pix0 + channels, 10);
+      // std::fill( pix1, pix1 + channels, 3);
+      // pix0[0] = 5;
+      // classifier.copy_pix(pix0, pix1);
+      // if (Transition::backward != classifier.f_classifier())
+      //     ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_OUTPUT);
+      //check that all values are between 1 and 1 (and not Nan)
+      // if (0 != cvtest::check(, 1, 1, 0) )
+      //     ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_TEST_DATA);
+
+  }
+};
+
+TEST(ComparatorLibSuite, f_classifierbt) {
+  FclassifierBT fclassifierbt;
+  fclassifierbt.safe_run();
 }
 
 
@@ -63,20 +110,13 @@ protected:
       int pix1[] = {10, 10, 10};
       
       classifier.copy_pix(pix0, pix1);
-      if (21 != classifier.light_distance())
-          ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_OUTPUT);
-      if (0 != classifier.color_distance())
-          ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_OUTPUT);
+      ASSERT_EQ(21, classifier.light_distance());
+      ASSERT_EQ(0, classifier.color_distance());
       
       pix1[0] = 13;
       classifier.copy_pix(pix0, pix1);
-      if (24 != classifier.light_distance())
-          ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_OUTPUT);
-      if (6 != classifier.color_distance())
-          ts->set_failed_test_info(cvtest::TS::FAIL_INVALID_OUTPUT);
-
-
-
+      ASSERT_EQ(24, classifier.light_distance());
+      ASSERT_EQ(6, classifier.color_distance());
   }
 };
 
