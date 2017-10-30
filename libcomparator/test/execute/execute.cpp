51bdf9c0f51d21cabea9c0f1982f0b842048d2d1
MOJNICK <szymonszozda@gmail.com>
2017-10-30 20:53:47 +0100

    light distance =>double 	modified:   ../lib/include/libcomparator.hpp 	modified:   ../lib/src/libcomparator.cpp 	modified:   ../test/execute/execute.cpp 	modified:   ../test/unit/comparatortest.cpp

--- libcomparator/test/execute/execute.cpp
+++ libcomparator/test/execute/execute.cpp
@@ -8,21 +8,22 @@
 int main( int argc, char** argv )
 {
 	cv::Mat image;
-    image = cv::imread(argv[1], CV_LOAD_IMAGE_COLOR);   // Read the file "/Downloads/reference_image.jpg"
+    image = cv::imread("/home/szozda/Downloads/reference_image.jpg", CV_LOAD_IMAGE_COLOR);   // Read the file "/Downloads/reference_image.jpg"
 
     if(! image.data )                              // Check for invalid input
     {
-    	std::cout<<"wrong/n";
+    	std::cout<<"wrong\n";
         return -1;
     }
 
     double factor = 0.25;
     cv::resize(image, image, cv::Size(), factor, factor);
 
+    TYPE acceptanceLevel = 1;
     double balance[] = {1.0, 1.0, 1.0};
-    double lightThreshold = 10.0;
-    double colorThreshold = 3.0;
-    IterateProcess<TYPE> iterateProcess(image, lightThreshold, colorThreshold, balance);
+    double lightThreshold = 1.1;
+    double colorThreshold = 8.0;
+    IterateProcess<TYPE> iterateProcess(image, acceptanceLevel, lightThreshold, colorThreshold, balance);
     auto result = iterateProcess.iterate_HV();
     DataProcess::concatenate_HV(result);
   
@@ -30,12 +31,16 @@ int main( int argc, char** argv )
     {
     	for_each(result.begin(), result.end(), [&image](auto el){
     		image.data[el.index] = 255;
-    		// image.data[el.index + 1] = 255; 
+    		image.data[el.index + 1] = 0; 
     		image.data[el.index + 2] = 255;
     	});
     	
 	    cv::namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
 	    cv::imshow( "Display window", image );                   // Show our image inside it.
+	}
+	else
+	{
+		std::cout<<"notcontionuous\n";
 	}
     cv::waitKey(0);                                          // Wait for a keystroke in the window
     return 0;
