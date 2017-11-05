6444ff6ca448c2817b3efe07c72ac07d57fce274
MOJNICK <szymonszozda@gmail.com>
2017-11-05 20:00:54 +0100

    modified:   ../test/execute/execute.cpp

--- libcomparator/test/execute/execute.cpp
+++ libcomparator/test/execute/execute.cpp
@@ -5,29 +5,59 @@
 #include "dataprocess.hpp"
 #include <iostream>
 #include "Python.h"
-
+#define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+// #include "ndarrayobject.h"
+#include "numpy/arrayobject.h"
 
 int main( int argc, char** argv )
 {
     {
-        PyObject *modname, *mymodule;           
-
         Py_Initialize();
+        
+        PyObject* osmodule = PyImport_ImportModule("os");
+        PyObject* sysmodule = PyImport_ImportModule("sys");        
+        
+        PyObject* syspath = PyObject_GetAttrString( sysmodule, "path" );
+        // PyObject_Print( syspath, stdout, 0 );
+        PyObject* ospath = PyObject_GetAttrString( osmodule, "path" );
+        PyObject* osgetcwd = PyObject_GetAttrString( osmodule, "getcwd" );
+        PyObject* osgetcwdresult = PyObject_CallObject( osgetcwd, NULL );
+        
+        PyObject* shadowstring = PyString_FromString("/../shadow");
+        PyObject* ospathdirnameresult = PyObject_CallMethod (ospath, (char *)"dirname", (char *)"O", osgetcwdresult );
 
-        PyRun_SimpleString("import sys");
-        PyImport_ImportModule("os");
-        PyRun_SimpleString("sys.path.append(os.path.join(os.path.dirname( os.getcwd()),'../shadow'))");
-        PyRun_SimpleString("print( sys.path )");
-        // PyRun_SimpleString("import clustering");
-        modname = PyString_FromString("numpy");
-        mymodule = PyImport_ImportModule("numpy");
-        // char* PyString_AsString(PyObject *string)
+
+        
+        // PyObject* tuplearg = Py_BuildValue("OO", ospathdirnameresult, shadowstring);
+        // PyObject* ospathjoinresult = PyObject_CallMethod( ospath, (char *)"join", (char *)"OO", ospathdirnameresult, shadowstring );
+        PyObject* list = PyList_New(0);
+        PyList_Append( list, ospathdirnameresult);
+        PyList_Append( list, shadowstring );
+        PyObject* emptystring = PyString_FromString("");
+        PyObject* ospathjoinresult = PyObject_CallMethod( emptystring, (char *)"join", (char *)"O", list );
+        
+        PyList_Append( syspath, ospathjoinresult );
+        
         
 
-        PyObject_Print(mymodule, stdout, 0);
+        // PyObject* dirnamestring = PyString_FromString("dirname");
+        // PyObject* ospathdirnameresult PyObject_CallMethodObjArgs(ospath, dirnamestring, ..., NULL)
+
+
+        // PyObject* ospathdirnameresult = PyObject_CallMethod(ospath, "dirname", "O", osgetcwdresult, shadowstring);
+        // PyRun_SimpleString("sys.path.append(os.path.join(os.path.dirname( os.getcwd()),'../shadow'))");
+        // PyObject* osgetcwdresult = PyObject_CallMethod(osmodule, "getcwd", NULL);
+        // char* PyString_AsString(PyObject *string)
+        // PyObject* modname = PyString_FromString("numpy");
+        
+
+        // PyRun_SimpleString("print( sys.path )");
+        PyObject* clusteringmodule = PyImport_ImportModule("clustering");
+
+        PyObject_Print(clusteringmodule, stdout, 0);
         PyObject *key, *value;
         Py_ssize_t pos = 0;
-        while (PyDict_Next(mymodule, &pos, &key, &value))
+        while (PyDict_Next(clusteringmodule, &pos, &key, &value))
         {
             // int i = PyInt_AS_LONG(value) + 1;
             char* strvalue = PyString_AsString(value);
@@ -38,23 +68,30 @@ int main( int argc, char** argv )
 
         size_t size = 12;
         int array[] = { 1,2,3,4,5,6,7,8,9,10,11,12 };
-        // PyObject *mymodule = PyImport_ImportModule( "clustering" );
-        PyObject *myfunc = PyObject_GetAttrString( mymodule, "CApi.call_points_clustering" );
-        if(myfunc == NULL){ return -1; }
-
         PyObject *mylist = PyList_New(size);
         for (size_t i = 0; i != size; ++i) {
             PyList_SET_ITEM(mylist, i, PyInt_FromLong(array[i]));
         }
-        PyObject *arglist = Py_BuildValue("(o)", mylist);
-        PyObject *result = PyObject_CallObject(myfunc, arglist);
+        PyObject *arglist = Py_BuildValue("O", mylist);
+
+        PyObject *clusteringCApi = PyObject_GetAttrString( clusteringmodule, "CApi" );
+        PyObject* myfunc = PyObject_GetAttrString( clusteringCApi, "call_points_clustering");
+        if(myfunc == NULL)
+            { return -1; }
+
+        Py_intptr_t dims = 1; //npy_intp
+        PyObject* numpyList = PyArray_SimpleNewFromData( 2 , &dims,  NPY_INT_32, array);
+        
+        PyObject *result = PyObject_CallObject(myfunc, mylist);
+        // PyObject_Print(result, stdout, 0);
         int retval = (int)PyInt_AsLong(result);
+
         Py_DECREF(result);
         Py_DECREF(arglist);
         Py_DECREF(mylist);
         Py_DECREF(myfunc);
-        Py_DECREF(mymodule);
-        return retval;
+        Py_DECREF(clusteringmodule);
+
 
         Py_Finalize();
     }
