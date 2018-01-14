2c0491bcda8d42939f19f79918bae5260f372869
MOJNICK <szymonszozda@gmail.com>
2018-01-14 15:59:24 +0100

    execute clean

---
+++ libcomparator/test/execute/show_result.hpp
@@ -0,0 +1,18 @@
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "libcomparator.hpp"
+#include "dataprocess.hpp"
+#include "clustering.hpp"
+
+void save_result(char* source_path, char const postFix[], char const outputFormat[], cv::Mat& image);
+
+void p_around(unsigned char* data, int before, int after);
+
+void draw_clusterNumber(cv::Mat& image, std::vector<IndexTransitionCluster> const & result);
+
+cv::Mat show_result(cv::Mat img, std::vector<IndexTransitionCluster> const & result );
+
+int test_on_image(char path[], double factor, double eps, uint minPts);
+
+int broad_HUE(char* path);
