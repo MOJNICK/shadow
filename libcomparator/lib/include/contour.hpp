5f6a354ea0fb25f3e300f367e4be38d08155c936
MOJNICK <szymonszozda@gmail.com>
2018-01-14 17:51:41 +0100

    envolving to canny edge

--- libcomparator/lib/include/contour.hpp
+++ libcomparator/lib/include/contour.hpp
@@ -1,23 +1,28 @@
 #include <opencv2/core/core.hpp>
 #include "libcomparator.hpp"
 
-class MatTransition
+class DataTransition
 {
+public:
 	unsigned char pixel;
 	Transition transition;
-}
+};
 
 class ContourTransition
 {
 public:
+	cv::Mat_<DataTransition> matTransition;
+	
 	ContourTransition( cv::Mat& image );
-
-	cv::Mat<MatTransition> matTransition;
 	void bw_push_transition(std::vector<IndexTransition> const & indexTransition);
-}
+
+private:
+	void copy_data( cv::Mat& image );
+	void set_transition_to_no();
+};
 
 class Preprocess
 {
 public:
 	cv::Mat get_thick_kernel( cv::Mat& image );
-}
+};
--- libcomparator/lib/src/contour.cpp
+++ libcomparator/lib/src/contour.cpp
@@ -2,18 +2,40 @@
 
 ContourTransition::ContourTransition( cv::Mat& image )
 {
-
+	copy_data( image );
 }
 
 void ContourTransition::bw_push_transition(std::vector<IndexTransition> const & indexTransition)
 {
-	std::for_each(result.begin(), result.end(), [ this ](auto el){
-        this->matTransition.data[el.bw_index( this->matTransition )].transition = el.transition;
+	std::for_each(indexTransition.begin(), indexTransition.end(), [ this ](auto& el){
+		matTransition( el.row, el.col ).transition = el.transition;
     });
 }
 
+void ContourTransition::copy_data( cv::Mat& image )
+{
+	set_transition_to_no();
+	matTransition.create( image.rows, image.cols );
+	for(int row = 0; row < matTransition.rows; row++ )
+		for(int col = 0; col < matTransition.cols; col++ )
+		{
+			matTransition(row, col).pixel = *image.ptr(row, col);
+		}
+}
+
+void ContourTransition::set_transition_to_no()
+{
+	for(int row = 0; row < matTransition.rows; row++ )
+		for(int col = 0; col < matTransition.cols; col++ )
+		{
+			matTransition(row, col).transition = Transition::no;
+		}
+}
+
+
+
 cv::Mat Preprocess::get_thick_kernel( cv::Mat& image )
 {
-	thickKernel = image.clone();
+	cv::Mat thickKernel = image.clone();
 	return thickKernel;
 }
