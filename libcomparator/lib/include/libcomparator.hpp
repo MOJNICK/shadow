7cbaa0608a69546364f06dc05594c912a9bfc6b4
MOJNICK <szymonszozda@gmail.com>
2018-05-18 10:51:10 +0200

    delete 2

--- libcomparator/lib/include/libcomparator.hpp
+++ libcomparator/lib/include/libcomparator.hpp
@@ -14,111 +14,8 @@
 		#endif
 	#endif
 
-	typedef unsigned int uint;
-	unsigned char const channels = 3;//not a parameter, only for convinience
-	typedef unsigned char TYPE;
-	typedef int DTYPE;
-	uint const distinctDir = 4;
-	uint const shiftToDistinct = 4;
-	uint const transDirCombi = 16;//4 distinct directions
-	uint const directionBit = 4;
-
-	enum Transition
-	{
-		empty = 0,
-		no = 1 << 0,
-		unknown = no << 1,
-
-		fwd = no << 2,
-		back = fwd << 1,
-		biFwdBack = fwd | back,
-
-		upToDw = no << directionBit,
-		lToR = upToDw << 1,
-		dwToUp = upToDw << 2,
-		rToL = upToDw << 3,
-
-		biLUp = lToR | upToDw,
-		biLDw = biLUp << 1,
-		biRDw = biLUp << 2,
-		biRUp = upToDw | rToL,
-		
-		biUpDw = upToDw | dwToUp,
-		biLR = biUpDw << 1,
-
-		triUp = rToL | upToDw | lToR,
-		triL = triUp << 1,
-		triDw = triUp << 2,
-		triR = triUp << 3,
-					
-		all = biLR | biUpDw
-
-	};
-
-	inline Transition operator|( Transition const & a, Transition const & b )
-	{ return static_cast< Transition >( static_cast< int >( a ) | static_cast< int >( b ) ); }
-
-	inline Transition& operator|=( Transition& a, Transition const & b )
-	{ return a = a | b; }
-
-	inline Transition operator<<( Transition const & a, uint const b )
-	{ return static_cast< Transition >( static_cast< uint >( a ) << b ); }
-
-	inline Transition& operator<<=( Transition& a, uint const b )
-	{ return a = a << b; }
-
-	class IndexTransition
-	{
-	public:
-		int row;
-		int col;
-		Transition transition;
-
-		//IndexTransition(IndexTransition const & indexTransition): row{indexTransition.row}, col{indexTransition.col}, transition{indexTransition.transition}{}
-		bool operator ==( IndexTransition const &it) const
-		{
-			if( this->row == it.row && this->col == it.col && this->transition == it.transition )
-			{
-				return true;
-			}
-			{
-				return false;
-			}
-		}
-
-		bool same_position( IndexTransition& b ) const
-		{
-			return ( ( row == b.row ) && ( col == b.col ) ) ? true : false;
-		}
-
-		uint index(cv::Mat& img)
-		{
-			return row * img.step + col * channels;
-		}
-
-		uint bw_index(cv::Mat& img)
-		{
-			return row * img.step + col;
-		}
-
-		uint conv_pix_to_subPix_col()
-		{
-			return col /= channels;
-		}
-
-		uint get_pix_to_subPix_col() const
-		{
-			return col / channels;
-		}
-
-		uint conv_subPix_to_pix_col()
-		{
-			return col *= channels;
-		}
-	};
+	typedef char unsigned UCHAR;
 	
-
-	class DataProcess;
 
 	template <class TYPE>
 	class Classifier;
@@ -128,15 +25,10 @@
 	class IterateProcess
 	{
 	public:
-		IterateProcess( cv::Mat&, TYPE, double, double, double[] );
-		std::vector<IndexTransition> iterate_HV();
+		IterateProcess();
+		double iterate_H(TYPE* pix0, TYPE* pix1);
 	private:		
-		cv::Mat_<TYPE> img;//reference by default?
 		Classifier<TYPE> classifier;
-
-		std::vector<IndexTransition> iterate_H();
-		std::vector<IndexTransition> iterate_V();
-		
 	};
 
 	
@@ -144,32 +36,8 @@
 	class Classifier
 	{
 	public:
-		Classifier( TYPE, double, double, double[] );
-		void copy_pix(TYPE[], TYPE[]);
-		Transition f_classifier();
-		#ifdef WITH_TESTS
-			void set_parameters( TYPE, double, double, double[] );
-		#endif
+		Classifier();
+		double color_distance(TYPE* pix0, TYPE* pix1);
 	private:
-		TYPE pix0[channels];
-		TYPE pix1[channels];
-		TYPE acceptanceLevel;
-		double lightThreshold;
-		double colorThreshold;
-		double colorBalance[channels];
-		double lightDistance;
-
-		void correct_pix0();
-		double color_distance();
-		double light_distance();
-
-		bool brighter();
-		void swap();
 	};
-
-	extern "C"
-	{
-
-	}
-
 #endif
--- libcomparator/lib/src/libcomparator.cpp
+++ libcomparator/lib/src/libcomparator.cpp
@@ -1,75 +1,28 @@
 #include "libcomparator.hpp"
 
-double const prealocate = 0.01;//vector reserve
+template <class TYPE> IterateProcess<TYPE>::IterateProcess(){}
+
+template <class TYPE> double IterateProcess<TYPE>::iterate_H(TYPE* pix0, TYPE* pix1)
+{
+	return classifier.color_distance(pix0, pix1);
+}
 
 
-	template <class TYPE> IterateProcess<TYPE>::IterateProcess(cv::Mat& img, TYPE acceptanceLevel, double lightThreshold, double colorThreshold, double colorBalance[]) : classifier(acceptanceLevel, lightThreshold, colorThreshold, colorBalance)
-	{
-		if(! img.isContinuous() )
-		{
-			img = cv::Mat( cv::Size(200,100), CV_8UC(channels), cv::Scalar(0));
-			cv::putText(img, "not coninuous", cv::Point(0,0), cv::FONT_HERSHEY_COMPLEX_SMALL, 0.8, cv::Scalar(200,200,250), 1, 8, false);
-			this->img = img;
-		}
-		else { this->img = img; }
-	}
+template <class TYPE> Classifier<TYPE>::Classifier(){}
 
-	template <class TYPE> std::vector<IndexTransition> IterateProcess<TYPE>::iterate_H()
-	{
-		std::vector<IndexTransition> result;
+template <class TYPE> double Classifier<TYPE>::color_distance(TYPE* pix0, TYPE* pix1)
+{
+	double var = -(pix1[0] / static_cast<double>(pix0[0]) + pix1[1] / static_cast<double>(pix0[1]) + pix1[2] / static_cast<double>(pix0[2]));//for minimize color_distance
+	return pow(pix1[0] / static_cast<double>(pix0[0]) + var, 2) + pow(pix1[1] / static_cast<double>(pix0[1]) + var, 2) + pow(pix1[2] / static_cast<double>(pix0[2]) + var, 2);
+}
 
-		for( int row = 0; row < img.rows; row++)		
-		{
-			int rowIndex = row * img.step;
-			classifier.copy_pix(img.data + rowIndex, img.data + rowIndex + channels);
-		}
-		return result;
-	}
 
-	template <class TYPE> Classifier<TYPE>::Classifier(TYPE acceptanceLevel_, double lightThreshold_, double colorThreshold_, double colorBalance_[]) : lightThreshold{lightThreshold_ + 1}, colorThreshold{colorThreshold_}
-	{
-		if(acceptanceLevel_ != 0){ acceptanceLevel = acceptanceLevel_; }
-		else{ acceptanceLevel = 1; }
-		memcpy(colorBalance, colorBalance_, sizeof(double) * channels);	
-	}
 
-	template <class TYPE> void Classifier<TYPE>::copy_pix(TYPE pix0_[], TYPE pix1_[])
-	{
-		memcpy(pix0, pix0_, sizeof(TYPE) * channels);
-		memcpy(pix1, pix1_, sizeof(TYPE) * channels);	
-	}
-
-	template <class TYPE> Transition Classifier<TYPE>::f_classifier()
-	{
-		Transition result = back;
-		if(color_distance() < colorThreshold)
-				return result;
-
-		return (Transition::no);
-	}
+void instantiate()
+{
+	UCHAR pix [] = {10,10,10};
+	Classifier<UCHAR> specifyCL;
 	
-	template <class TYPE> double Classifier<TYPE>::color_distance()
-	{
-		double var = -(pix1[0] / static_cast<double>(pix0[0]) + pix1[1] / static_cast<double>(pix0[1]) + pix1[2] / static_cast<double>(pix0[2])) / channels;//for minimize color_distance
-		return pow(pix1[0] / static_cast<double>(pix0[0]) + var, 2) + pow(pix1[1] / static_cast<double>(pix0[1]) + var, 2) + pow(pix1[2] / static_cast<double>(pix0[2]) + var, 2);
-	}
-
-
-
-	void instantiate()
-	{
-		double x[]={1.2,1.0,1.0};
-		TYPE pix [] = {10,10,10};
-		Classifier<TYPE> specifyCL(1, 1.0, 1.0, x);
-		specifyCL.copy_pix(pix, pix);
-		specifyCL.f_classifier();
-		cv::Mat_<TYPE> mat(0,0);
-		IterateProcess<TYPE> specifyIT( mat, 1, 1.0, 1.0, x );
-	#ifdef WITH_TESTS
-		specifyCL.set_parameters(1, 1.0, 1.0, x);
-		#ifdef TEST_PRIVATE_PART
-			specifyIT.iterate_H();
-			specifyIT.iterate_V();
-		#endif
-	#endif
-	}
+	IterateProcess<UCHAR> specifyIT;
+	specifyIT.iterate_H(pix, pix);
+}
