817f9685ba2962a1d7ad5e79f99b00d48d74af4c
MOJNICK <szymonszozda@gmail.com>
2018-05-18 11:06:29 +0200

    delete 3

--- libcomparator/lib/include/libcomparator.hpp
+++ libcomparator/lib/include/libcomparator.hpp
@@ -1,43 +1,17 @@
 #ifndef COMPARATOR_HPP
 	#define COMPARATOR_HPP
-	#include <cstring>
-	#include <opencv2/core/core.hpp>
-	#include "math.h"
-	#include <algorithm>
-	#include <list>
-	#include <vector>
-	#include <utility>
-
-	#ifdef WITH_TESTS
-		#ifdef TEST_PRIVATE_PART
-			#define private public
-		#endif
-	#endif
+	#include <cmath>
 
 	typedef char unsigned UCHAR;
 	
-
-	template <class TYPE>
-	class Classifier;
-
-
 	template <class TYPE>
 	class IterateProcess
 	{
 	public:
 		IterateProcess();
 		double iterate_H(TYPE* pix0, TYPE* pix1);
+		double color_distance(TYPE* pix0, TYPE* pix1);
 	private:		
-		Classifier<TYPE> classifier;
 	};
 
-	
-	template <class TYPE>
-	class Classifier
-	{
-	public:
-		Classifier();
-		double color_distance(TYPE* pix0, TYPE* pix1);
-	private:
-	};
 #endif
--- libcomparator/lib/src/libcomparator.cpp
+++ libcomparator/lib/src/libcomparator.cpp
@@ -4,24 +4,18 @@ template <class TYPE> IterateProcess<TYPE>::IterateProcess(){}
 
 template <class TYPE> double IterateProcess<TYPE>::iterate_H(TYPE* pix0, TYPE* pix1)
 {
-	return classifier.color_distance(pix0, pix1);
+	return color_distance(pix0, pix1);
 }
 
-
-template <class TYPE> Classifier<TYPE>::Classifier(){}
-
-template <class TYPE> double Classifier<TYPE>::color_distance(TYPE* pix0, TYPE* pix1)
+template <class TYPE> double IterateProcess<TYPE>::color_distance(TYPE* pix0, TYPE* pix1)
 {
 	double var = -(pix1[0] / static_cast<double>(pix0[0]) + pix1[1] / static_cast<double>(pix0[1]) + pix1[2] / static_cast<double>(pix0[2]));//for minimize color_distance
-	return pow(pix1[0] / static_cast<double>(pix0[0]) + var, 2) + pow(pix1[1] / static_cast<double>(pix0[1]) + var, 2) + pow(pix1[2] / static_cast<double>(pix0[2]) + var, 2);
+	return std::pow(pix1[0] / static_cast<double>(pix0[0]) + var, 2) + std::pow(pix1[1] / static_cast<double>(pix0[1]) + var, 2) + std::pow(pix1[2] / static_cast<double>(pix0[2]) + var, 2);
 }
-
-
 
 void instantiate()
 {
 	UCHAR pix [] = {10,10,10};
-	Classifier<UCHAR> specifyCL;
 	
 	IterateProcess<UCHAR> specifyIT;
 	specifyIT.iterate_H(pix, pix);
