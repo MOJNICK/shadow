24a4cb76ae67fb885ed8a47c49c0d462219b53bb
MOJNICK <szymonszozda@gmail.com>
2018-08-16 21:58:09 +0200

    resize before norm, compare script clean

--- compare_script.sh
+++ compare_script.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
+set -e
+make -j8
+
 filePairs=$1
 removedDirectory=$2
 nakedDirectory=$3
 
-make -j8
-
-while read pair
-	do
-		read removed naked<<<"$pair"
-		removed=$removedDirectory$removed
-		naked=$nakedDirectory$naked
-	done < $filePairs
-
-parallel -k -a $filePairs --colsep '\t' ./test/execute/execute --relative_norm $removedDirectory{1} $nakedDirectory{2} >> compare_script.out
-
+parallel -k -a $filePairs --colsep '\t' ./test/execute/execute --relative_norm $removedDirectory{1} $nakedDirectory{2} >> compare_script.tmp
+cp compare_script.out compare_script.tmp.2
+paste -d'\n' compare_script.tmp.2 compare_script.tmp > compare_script.out
+rm compare_script.tmp compare_script.tmp.2
 
 exit 0
